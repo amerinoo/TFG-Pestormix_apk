fedca29866e3adbd6ec0edf64ab24a5d83e94bfd
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-01-23 14:34:26 +0100

    Changed main colors. Created SplashActivity

---
+++ app/src/main/java/com/example/albert/pestormix_apk/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.example.albert.pestormix_apk.activities;
+
+import android.os.Bundle;
+
+import com.example.albert.pestormix_apk.R;
+import com.example.albert.pestormix_apk.application.PestormixMaster;
+
+public class SplashActivity extends PestormixMaster {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
