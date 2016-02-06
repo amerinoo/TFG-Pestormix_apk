637ec45fd7ec525ad201d6e18b0205dff8d97069
Albert Eduard Merino Pulido <albertmerino11@gmail.com>
2016-02-06 13:12:07 +0100

    Reduce text and remove de background of name

--- app/src/main/java/com/example/albert/pestormix_apk/fragments/ScreenSlidePageFragment.java
+++ app/src/main/java/com/example/albert/pestormix_apk/fragments/ScreenSlidePageFragment.java
@@ -2,7 +2,6 @@ package com.example.albert.pestormix_apk.fragments;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,7 +41,7 @@ public class ScreenSlidePageFragment extends PestormixMasterFragment implements 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_valves, container, false);
+        return inflater.inflate(R.layout.fragment_slide_drinks, container, false);
     }
 
     @Override
